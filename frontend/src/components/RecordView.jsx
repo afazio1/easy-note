9a2966c56d2139f4bc3e699ba35b80ded1975389
armanLatefi <armanlatefi22@gmail.com>
2023-10-15 03:12:04 -0400

    yo its arman

--- frontend/src/components/RecordView.jsx
+++ frontend/src/components/RecordView.jsx
@@ -29,21 +29,41 @@ const Dictaphone = () => {
     )
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
-      <p className='lg:text-3xl my-5 text-white'>Microphone: {listening ? 'on' : 'off'}</p>
-      <nav className="bg-lighter-grey p-4">
-        <div className="container mx-auto flex justify-center items-center">
+      <h1 className='font-custom font-bold text-white lg:text-5xl flex justify-center p-12'>Easy Note</h1>
+      {/* <p className='font-custom lg:text-3xl my-5 text-white flex justify-center'>Microphone: {listening ? 'on' : 'off'}</p> */}
+      <nav className="w-9/10 rounded-lg bg-blue-400 container mx-auto p-4 flex justify-between items-center ">
+        <div className="font-custom container mx-auto flex justify-center items-center">
             <div className="space-x-10">
               {/* Start, Stop, Reset */}
-              <button className="text-white hover:text-blue-200" onClick={handleStart}>Start</button>
-              <button className="text-white hover:text-blue-200" onClick={SpeechRecognition.stopListening}>Stop</button>
-              <button className="text-white hover:text-blue-200" onClick={resetTranscript}>Reset</button>
-              <button className="text-white hover:text-blue-200" onClick={handleToggle}>Enhance with AI</button>
+              <button className="lg:text-2xl font-custom text-white hover:text-blue-200" onClick={handleStart}>Start</button>
+              <button className="lg:text-2xl font-custom text-white hover:text-blue-200" onClick={SpeechRecognition.stopListening}>Stop</button>
+              <button className="lg:text-2xl font-custom text-white hover:text-blue-200" onClick={resetTranscript}>Reset</button>
+              <button className="lg:text-2xl font-custom text-white hover:text-blue-200" onClick={handleToggle}>Enhance with AI</button>
             </div>
         </div>
       </nav>
-      <p className='text-white lg:text-2xl my-5'>{transcript}</p>
+      <p className='text-center font-custom lg:text-3xl text-white flex justify-center py-8'>Microphone: {listening ? 'On' : 'Off'}</p>
+      <p className='font-custom text-white lg:text-2xl my-5 py-10 px-60'>{transcript}</p>
     </div>
   );
 };
