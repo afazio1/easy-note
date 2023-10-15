c7963dd55f26894a53d76793db600e3d56c44b31
armanLatefi <armanlatefi22@gmail.com>
2023-10-14 20:18:08 -0400

    Slight changes to the syling

--- frontend/src/components/RecordView.jsx
+++ frontend/src/components/RecordView.jsx
@@ -16,12 +16,19 @@ const Dictaphone = () => {
   }
 
   return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+    <div class="not-live">
+      <p className='lg:text-3xl my-5 text-white'>Microphone: {listening ? 'on' : 'off'}</p>
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto flex justify-center items-center">
+            <div className="space-x-10">
+              {/* Start, Stop, Reset */}
+              <button className="text-white hover:text-blue-200" onClick={handleStart}>Start</button>
+              <button className="text-white hover:text-blue-200" onClick={SpeechRecognition.stopListening}>Stop</button>
+              <button className="text-white hover:text-blue-200" onClick={resetTranscript}>Reset</button>
+            </div>
+        </div>
+      </nav>
+      <p className='text-white lg:text-xl my-5'>{transcript}</p>
     </div>
   );
 };
